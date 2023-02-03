cd9b99828132605ecab2dfb984f26f56f9718f9c
Brian Kinsella <brian.kinsella@distilled.ie>
2023-02-03 22:03:20 +0000

    added temporary deploy script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+***REMOVED***
+
+export AWS_PROFILE=ses-brian
+***REMOVED***
+export GITHUB_TOKEN=***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+#***REMOVED***
+#***REMOVED***
+#***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+
+
