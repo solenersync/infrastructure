898d6d90a5035559f98c8ab11e1c40e6f9d89150
Brian Kinsella <brian.kinsella@distilled.ie>
2023-02-05 12:33:39 +0000

    for testing

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,9 @@ flux bootstrap github \
   --components-extra=image-reflector-controller,image-automation-controller \
   --read-write-key=true \
   --path=clusters/staging
-#wait
-#flux reconcile kustomization flux-system --with-source
-#wait
+wait
+flux reconcile kustomization flux-system --with-source
+wait
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/aws/deploy.yaml
 wait
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.yaml
