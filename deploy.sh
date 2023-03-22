0bc49b784d30d8166287766f64c61872059f0203
Brian Kinsella <>
2023-03-22 15:04:43 +0000

    remove gt

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,23 @@
-***REMOVED***
+#!/bin/bash
 
-export AWS_PROFILE=ses-admin
-***REMOVED***
-export GITHUB_TOKEN=***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+aws eks update-kubeconfig --region eu-west-1 --name staging
+wait
+flux check --pre
+wait
+flux bootstrap github \
+  --owner=solenersync \
+  --repository=infrastructure \
+  --components-extra=image-reflector-controller,image-automation-controller \
+  --read-write-key=true \
+  --path=clusters/staging
+wait
+flux reconcile kustomization flux-system --with-source
+wait
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.5.1/deploy/static/provider/aws/deploy.yaml
+wait
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.yaml
+wait
+flux reconcile kustomization flux-system --with-source
 
 
 
